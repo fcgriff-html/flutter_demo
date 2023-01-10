ec211eb42810596fae084ba0c169d4e7fd74f100
Frederick Griffin <fcgriffin2@outlook.com>
2023-01-10 15:33:54 -0500

    updating a bunch of files and creating home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_app/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,14 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: const LoginPage(),
       debugShowCheckedModeBanner: false,
@@ -28,10 +27,65 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Griffin App demo'),
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 55,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'To Griff Blog',
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Image.asset('images/login.png'),
+            const SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () {
+                debugPrint('Login Button Pressed');
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (BuildContext context) {
+                      return const HomePage();
+                    },
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.red,
+                minimumSize: const Size(300, 40),
+              ),
+              child: const Text('Login'),
+            ),
+            const SizedBox(height: 10.0),
+            ElevatedButton(
+              onPressed: () {
+                debugPrint('Register Button Pressed');
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.redAccent,
+                foregroundColor: Colors.white,
+                minimumSize: const Size(300, 40),
+              ),
+              child: const Text('Register'),
+            ),
+          ],
+        ),
       ),
-      
     );
   }
 }
